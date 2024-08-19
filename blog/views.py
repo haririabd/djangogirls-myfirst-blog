36fb2e1cffc3584ce0601f09b3b842ba9740e34e
haririabd <hariri.bin.abdullah@gmail.com>
2024-08-20 00:17:06 +0800

    Added views and template for single post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post #We import Post model to be use im here
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
@@ -8,3 +9,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # We pass our QuerySet above to the template by assigning the querypost to the template as posts
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
